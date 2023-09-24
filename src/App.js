5b0917c99aff6fe510a3069a917920855cba3583
vansh <thevanshds27@gmail.com>
2023-09-24 14:33:30 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './pages/Home';
 
 function App() {
+  // Initialize group and order from local storage or use default values
+  const [group, setGroup] = useState(localStorage.getItem('group') || 'Status');
+  const [order, setOrder] = useState(localStorage.getItem('order') || 'Title');
 
-  const [group, setgroup] = useState("Status");
-  const [order, setorder] = useState("Title");
+  // Update local storage whenever group or order changes
+  useEffect(() => {
+    localStorage.setItem('group', group);
+    localStorage.setItem('order', order);
+  }, [group, order]);
 
   return (
     <>
-      <Header group={group} setgroup={setgroup} order={order} setorder={setorder} />
+      <Header group={group} setgroup={setGroup} order={order} setorder={setOrder} />
       <Home group={group} order={order} />
     </>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import Deck from '../components/Deck'
+// Home.js
+
+import React, { useEffect, useState } from 'react';
+import Deck from '../components/Deck';
 import Userprofile from '../components/Userprofile';
-import './home.css'
-import { Priority, status } from '../constants/data.js'
-import { useState, useEffect } from 'react'
+import './home.css';
+import { Priority, status } from '../constants/data.js';
 
 const Home = ({ group, order }) => {
-  const [record, setrecord] = useState(null);
+  const [record, setRecord] = useState(null);
 
   useEffect(() => {
     fetch("https://api.quicksell.co/v1/internal/frontend-assignment")
       .then(response => response.json())
-      .then(data => setrecord(data))
+      .then(data => setRecord(data))
       .catch(err => console.log(err))
   }, []);
 
@@ -23,11 +24,11 @@ const Home = ({ group, order }) => {
       {group === "Priority" && Priority.map((item, id) => (
         <Deck key={id} symbol={<item.Symbol />} decktitle={item.title} data={record} group={group} order={order} />
       ))}
-      {group === "User" && record.users.map((item, id) => (
+      {group === "User" && record && record.users && record.users.map((item, id) => (
         <Deck key={id} symbol={<Userprofile firstletter={item.name[0]} available={item.available} />} decktitle={item.name} uid={item.id} data={record} group={group} order={order} />
       ))}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
