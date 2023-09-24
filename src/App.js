298e15c07e135445f5856d59f130b143df566adf
vansh <thevanshds27@gmail.com>
2023-09-24 19:32:37 +0530

    font update

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import Header from './components/Header';
 import Home from './pages/Home';
 
 function App() {
-  // Initialize group and order from local storage or use default values
   const [group, setGroup] = useState(localStorage.getItem('group') || 'Status');
   const [order, setOrder] = useState(localStorage.getItem('order') || 'Title');
 
-  // Update local storage whenever group or order changes
   useEffect(() => {
     localStorage.setItem('group', group);
     localStorage.setItem('order', order);
